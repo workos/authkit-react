e0306dba121d0da2c650fdae664b5163406bad67
Cameron Matheson <cameron@workos.com>
2024-08-02 17:53:32 -0600

    Update context after refreshing
    This makes sure that apps respond to changes to the User or claims in
    the access token.

--- src/provider.tsx
+++ src/provider.tsx
@@ -1,10 +1,14 @@
-'use client';
+"use client";
 
-import * as React from 'react';
-import { createClient } from '@workos-inc/authkit-js';
-import { Context } from './context';
-import { Client, CreateClientOptions } from './types';
-import { initialState } from './state';
+import * as React from "react";
+import {
+  AuthenticationResponse,
+  createClient,
+  getClaims,
+} from "@workos-inc/authkit-js";
+import { Context } from "./context";
+import { Client, CreateClientOptions } from "./types";
+import { initialState } from "./state";
 
 interface AuthKitProviderProps extends CreateClientOptions {
   clientId: string;
@@ -12,20 +16,49 @@ interface AuthKitProviderProps extends CreateClientOptions {
 }
 
 export function AuthKitProvider(props: AuthKitProviderProps) {
-  const { clientId, devMode, apiHostname, https, port, redirectUri, children, onRedirectCallback } = props;
+  const {
+    clientId,
+    devMode,
+    apiHostname,
+    https,
+    port,
+    redirectUri,
+    children,
+    onRedirectCallback,
+  } = props;
   const [client, setClient] = React.useState<Client>(NOOP_CLIENT);
   const [state, setState] = React.useState(initialState);
+
+  const onRefresh = React.useCallback(
+    ({ user, accessToken, organizationId }: AuthenticationResponse) => {
+      const { role = null, permissions = [] } = getClaims(accessToken);
+      setState((prev) => ({
+        ...prev,
+        user,
+        organizationId: organizationId ?? null,
+        role,
+        permissions,
+      }));
+    },
+    [client]
+  );
 
   React.useEffect(() => {
     function initialize() {
       const timeoutId = setTimeout(() => {
-        createClient(clientId, { apiHostname, port, https, redirectUri, devMode, onRedirectCallback }).then(
-          (client) => {
-            const user = client.getUser();
-            setClient(client);
-            setState((prev) => ({ ...prev, isLoading: false, user }));
-          },
-        );
+        createClient(clientId, {
+          apiHostname,
+          port,
+          https,
+          redirectUri,
+          devMode,
+          onRedirectCallback,
+          onRefresh,
+        }).then(async (client) => {
+          const user = client.getUser();
+          setClient(client);
+          setState((prev) => ({ ...prev, isLoading: false, user }));
+        });
       });
 
       return () => {
@@ -39,7 +72,11 @@ export function AuthKitProvider(props: AuthKitProviderProps) {
     return initialize();
   }, [clientId, apiHostname, https, port, redirectUri]);
 
-  return <Context.Provider value={{ ...client, ...state }}>{children}</Context.Provider>;
+  return (
+    <Context.Provider value={{ ...client, ...state }}>
+      {children}
+    </Context.Provider>
+  );
 }
 
 const NOOP_CLIENT: Client = {
